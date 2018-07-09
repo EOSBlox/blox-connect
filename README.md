5bb99b651c6bc87ef2c9c035ecffd45187217d8e
Will in China <email.will.in.china@gmail.com>
2018-07-09 18:36:41 +0800

    add: working version

---
+++ README.md
@@ -0,0 +1,38 @@
+# \<blox-connect\>
+
+A webcomponent that connects to the EOS blockchain
+
+## Install the Polymer-CLI
+
+First, make sure you have the [Polymer CLI](https://www.npmjs.com/package/polymer-cli) and npm (packaged with [Node.js](https://nodejs.org)) installed. Run `npm install` to install your element's dependencies, then run `polymer serve` to serve your element locally.
+
+## Install blox-paper
+
+```
+$ npm install blox-connect
+```
+
+## Viewing Your Element
+
+```
+$ polymer serve
+```
+
+## Running Tests
+
+```
+$ polymer test
+```
+
+## Import
+
+```
+$ import 'blox-connect';
+```
+
+## Basic Use
+
+```html
+<blox-connect selector eos="{{eos}}">
+</blox-connect>
+```
---
+++ blox-connect.js
@@ -0,0 +1,109 @@
+import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import {} from './eos.js';
+
+/**
+ * `blox-connect`
+ * A webcomponent that connects to the EOS blockchain
+ *
+ * @customElement
+ * @polymer
+ * @demo demo/index.html
+ */
+class BloxConnect extends PolymerElement {
+  static get template() {
+    return html`
+      <style>
+        :host {
+          display: block;
+        }
+        .selector {
+          width: 100%;
+          border-color: #d1d5d7;
+          -webkit-appearance: none;
+          appearance: none;
+          -moz-appearance: none;
+          -moz-appearance: none;
+          line-height: 15px;
+          padding: 5px 5px 5px 7px;
+          -webkit-padding-end: 25px;
+          height: 40px;
+          font-size: 13px;
+          border: 1px solid #D0D3D7;
+          border-radius: 4px;
+          background: url(data:image/gif;base64,R0lGODlhEgAWALMAAI2UoJKapXqAiH6DjHyCioyTno+WodHT1X+EjdDR07/BxP///////wAAAAAAAAAAACH5BAEAAAwALAAAAAASABYAAARWMKlJ60wsM7W6/52ibWC5iBpnfmimruH4wi35EQRYz0MQDCyZRwAwGAACzw6mFDJPTuayIxggEINkLPUB+ABB7qdQ0EVp59VUmjat0ZqDZa44jO74fAQAOw==) no-repeat;
+          background-position: 99% 50%;
+          background-size: 18px 22px;
+          outline:0;
+          background-color: #F0F1F3;
+        }
+      </style>
+      <template is="dom-if" if="{{selector}}">
+        <select on-change="_selected" id="select" class="selector">
+          <option value="eosNewYork-mainNet">EOS New York - Main Net</option>
+          <option value="jungle-testNet">Jungle - Test Net</option>
+        </select>
+      </template>
+    `;
+  }
+  static get properties() {
+    return {
+      eos: {
+        type: Object,
+        notify: true,
+        reflectToAttribute: true
+      },
+      keyProvider: {
+        type: String,
+      },
+      httpEndpoint: {
+        type: String,
+        value: "https://api.eosnewyork.io",
+      },
+      chainId: {
+        type: String,
+        value: "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906"
+      },
+      selector: {
+        type: Boolean,
+        value: false,
+      },
+      expireInSeconds: {
+        type: Number,
+        value: 30,
+      }
+    };
+  }
+
+  ready() {
+    super.ready();
+    this._connect();
+  }
+
+  _connect(keyProvider, httpEndpoint, expireInSeconds){
+    return new Promise((resolve, reject) => {
+      const config = {
+        keyProvider: keyProvider || this.keyProvider, 
+        httpEndpoint: httpEndpoint || this.httpEndpoint,
+        broadcast: true,
+        sign: true,
+        chainId: this.chainId,
+        expireInSeconds: expireInSeconds || this.expireInSeconds
+      }
+      this.eos = Eos.Localnet(config)
+      resolve(this.eos);
+    })
+  }
+
+  _selected(){
+  this.selected = this.shadowRoot.querySelector('#select').value
+    if (this.selected === "eosNewYork-mainNet") {
+      this.httpEndpoint = "https://api.eosnewyork.io";
+      this.chainId = "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906";
+    } else if (this.selected === "eosNewYork-mainNet") {
+      this.httpEndpoint = "https://dev.cryptolions.io:38888";
+      this.chainId = "038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca";
+    }
+    this._connect()
+  }
+
+} window.customElements.define('blox-connect', BloxConnect);
